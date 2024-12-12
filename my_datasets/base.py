e01cc3728f5539720bc8339e050f2bbeae27919a
Kenny3Shen <asdjkl740886@outlook.com>
2024-12-12 23:58:19 +0800

    DiffSo init

---
+++ my_datasets/base.py
@@ -0,0 +1,310 @@
+import os
+import random
+from pathlib import Path
+
+import Augmentor
+import cv2
+import numpy as np
+import torchvision.transforms.functional as TF
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+def convert_image_to_fn(img_type, image):
+    if image.mode != img_type:
+        return image.convert(img_type)
+    return image
+
+class Dataset(Dataset):
+    def __init__(
+        self,
+        folder,
+        image_size,
+        exts=['jpg', 'jpeg', 'png', 'tiff'],
+        augment_flip=False,
+        convert_image_to=None,
+        condition=0,
+        equalizeHist=False,
+        crop_patch=True,
+        halftone=None,
+        gaussian_filter=False,
+        get_sobel=None,
+        sample=False
+    ):
+        super().__init__()
+        self.equalizeHist = equalizeHist
+        self.exts = exts
+        self.augment_flip = augment_flip
+        self.condition = condition
+        self.crop_patch = crop_patch
+        self.halftone = halftone
+        self.gaussian_filter = gaussian_filter
+        self.get_sobel = get_sobel
+        self.sample = sample
+        if condition == 1:
+            # condition
+            self.gt = self.load_flist(folder[0])
+            self.input = self.load_flist(folder[1])
+        elif condition == 0:
+            # generation
+            self.paths = self.load_flist(folder)
+        elif condition == 2:
+            self.gt = self.load_flist(folder[0])
+            self.input = self.load_flist(folder[1])
+            self.input_condition = self.load_flist(folder[2])
+
+        self.image_size = image_size
+        self.convert_image_to = convert_image_to
+
+    def __len__(self):
+        if self.condition:
+            return len(self.input)
+        else:
+            return len(self.paths)
+
+    def __getitem__(self, index):
+        if self.condition == 1:
+            # condition
+            img0 = Image.open(self.gt[index])
+            img1 = Image.open(self.input[index])
+            w, h = img0.size
+            img0 = convert_image_to_fn(
+                self.convert_image_to, img0) if self.convert_image_to else img0
+            img1 = convert_image_to_fn(
+                self.convert_image_to, img1) if self.convert_image_to else img1
+
+            img0, img1 = self.pad_img([img0, img1], self.image_size)
+
+            if self.crop_patch and not self.sample:
+                img0, img1 = self.get_patch([img0, img1], self.image_size)
+
+            img1 = self.cv2equalizeHist(img1) if self.equalizeHist else img1
+
+            images = [[img0, img1]]
+            p = Augmentor.DataPipeline(images)
+            if self.augment_flip:
+                p.flip_left_right(1)
+            if not self.crop_patch:
+                p.resize(1, self.image_size, self.image_size)
+                # p.crop_by_size(1, self.image_size, self.image_size, centre=False)
+            g = p.generator(batch_size=1)
+            augmented_images = next(g)
+            img0 = cv2.cvtColor(augmented_images[0][0], cv2.COLOR_BGR2RGB)
+            img1 = cv2.cvtColor(augmented_images[0][1], cv2.COLOR_BGR2RGB)
+
+            return [self.to_tensor(img0), self.to_tensor(img1)]
+        elif self.condition == 0:
+            # generation
+            path = self.paths[index]
+            img = Image.open(path)
+            img = convert_image_to_fn(
+                self.convert_image_to, img) if self.convert_image_to else img
+
+            img = self.pad_img([img], self.image_size)[0]
+
+            if self.crop_patch and not self.sample:
+                img = self.get_patch([img], self.image_size)[0]
+
+            img = self.cv2equalizeHist(img) if self.equalizeHist else img
+
+            images = [[img]]
+            p = Augmentor.DataPipeline(images)
+            if self.augment_flip:
+                p.flip_left_right(1)
+            if not self.crop_patch:
+                p.resize(1, self.image_size, self.image_size)
+            g = p.generator(batch_size=1)
+            augmented_images = next(g)
+            img = cv2.cvtColor(augmented_images[0][0], cv2.COLOR_BGR2RGB)
+
+            return self.to_tensor(img)
+        elif self.condition == 2:
+            # condition
+            img0 = Image.open(self.gt[index])
+            img1 = Image.open(self.input[index])
+            img2 = Image.open(self.input_condition[index])
+            img0 = convert_image_to_fn(
+                self.convert_image_to, img0) if self.convert_image_to else img0
+            img1 = convert_image_to_fn(
+                self.convert_image_to, img1) if self.convert_image_to else img1
+            img2 = convert_image_to_fn(
+                self.convert_image_to, img2) if self.convert_image_to else img2
+
+            img0, img1, img2 = self.pad_img(
+                [img0, img1, img2], self.image_size)
+
+            if self.crop_patch and not self.sample:
+                img0, img1, img2 = self.get_patch(
+                    [img0, img1, img2], self.image_size)
+
+            # halftone 
+            img1 = self.ht(img1) if self.halftone else img1
+
+            # gaussian filter
+            img1 = self.cv2gaussian_filter(img1) if self.gaussian_filter else img1
+
+            # equalize histogram
+            img1 = self.cv2equalizeHist(img1) if self.equalizeHist else img1
+
+            # weighted sobel
+            img2 = self.cv2weightedSobel(img2) if self.get_sobel else img2    
+            images = [[img0, img1, img2]]
+            p = Augmentor.DataPipeline(images)
+            if self.augment_flip:
+                p.flip_left_right(1)
+            if not self.crop_patch:
+                #p.resize(1, self.image_size, self.image_size)
+                p.crop_by_size(1, self.image_size, self.image_size, centre=False)
+            g = p.generator(batch_size=1)
+            augmented_images = next(g)
+            img0 = cv2.cvtColor(augmented_images[0][0], cv2.COLOR_BGR2RGB)
+            img1 = cv2.cvtColor(augmented_images[0][1], cv2.COLOR_BGR2RGB)
+            img2 = cv2.cvtColor(augmented_images[0][2], cv2.COLOR_BGR2RGB)
+
+            return [self.to_tensor(img0), self.to_tensor(img1), self.to_tensor(img2)]
+
+    def load_flist(self, flist):
+        if isinstance(flist, list):
+            return flist
+
+        # flist: image file path, image directory path, text file flist path
+        if isinstance(flist, str):
+            if os.path.isdir(flist):
+                # file_list = [p for ext in self.exts for p in Path(f'{flist}').glob(f'**/*.{ext}')]
+                # print(file_list)  # 打印列表
+                # return file_list
+                
+                p = []
+                for root, dirs, files in os.walk(Path(f'{flist}')):
+                    for file in files:
+                        if file.endswith(tuple(self.exts)):
+                            p.append(Path(root) / file)  # 使用 Path 类构建路径
+                #print(p)
+                return p
+                            
+            if os.path.isfile(flist):
+                try:
+                    return np.genfromtxt(flist, dtype=np.str, encoding='utf-8')
+                except:
+                    return [flist]
+
+        return []
+    
+    def ht(self, img):
+        if self.halftone == 'fs':
+            (b, g, r) = cv2.split(img)
+            b = Image.fromarray(b).convert('1').convert('L')
+            g = Image.fromarray(g).convert('1').convert('L')
+            r = Image.fromarray(r).convert('1').convert('L')
+            img = cv2.merge((np.array(b), np.array(g), np.array(r)))
+        elif self.halftone == 'evcs':
+            (b, g, r) = cv2.split(img_bgr)
+            b, g, r = (b / 4).astype(np.uint8), (g / 4).astype(np.uint8), (r / 4).astype(np.uint8)
+            b = Image.fromarray(b).convert('1').convert('L')
+            g = Image.fromarray(g).convert('1').convert('L')
+            r = Image.fromarray(r).convert('1').convert('L')
+            img = cv2.merge((np.array(b), np.array(g), np.array(r)))
+        elif self.halftone == 'gmevcs':
+            img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            img_hsv[:, :, 2] = (img_hsv[:, :, 2] / 4).astype(np.uint8)
+            img_bgr = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
+            (b, g, r) = cv2.split(img_bgr)
+            b = Image.fromarray(b).convert('1').convert('L')
+            g = Image.fromarray(g).convert('1').convert('L')
+            r = Image.fromarray(r).convert('1').convert('L')
+            img = cv2.merge((np.array(b), np.array(g), np.array(r)))
+        return img
+
+    def cv2equalizeHist(self, img):
+        (b, g, r) = cv2.split(img)
+        b = cv2.equalizeHist(b)
+        g = cv2.equalizeHist(g)
+        r = cv2.equalizeHist(r)
+        img = cv2.merge((b, g, r))
+        return img
+    
+    def cv2gaussian_filter(self, img):
+        img = cv2.GaussianBlur(img, (3, 3), 0)
+        return img
+
+    def cv2weightedSobel(self, img):
+        if self.get_sobel == 'wsobel':
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img_sobel_x = cv2.Sobel(img_gray, cv2.CV_64F, 1, 0, ksize=3)
+            img_sobel_x = cv2.convertScaleAbs(img_sobel_x)
+            img_sobel_y = cv2.Sobel(img_gray, cv2.CV_64F, 0, 1, ksize=3)
+            img_sobel_y = cv2.convertScaleAbs(img_sobel_y)
+            img_sobel = cv2.addWeighted(img_sobel_x, 0.5, img_sobel_y, 0.5, 0)
+        elif self.get_sobel == 'sobel':
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img_sobel = cv2.Sobel(img_gray, cv2.CV_64F, 1, 1, ksize=3)
+        elif self.get_sobel == 'canny':
+            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img_sobel = cv2.Canny(img_gray, 100, 200)
+        return img_sobel
+    def to_tensor(self, img):
+        img = Image.fromarray(img)  # returns an image object.
+        img_t = TF.to_tensor(img).float()
+        return img_t
+
+    def load_name(self, index, sub_dir=False):
+        if self.condition:
+            # condition
+            name = self.input[index]
+            if sub_dir == 0:
+                return os.path.basename(name)
+            elif sub_dir == 1:
+                path = os.path.dirname(name)
+                sub_dir = (path.split("/"))[-1]
+                return sub_dir+"_"+os.path.basename(name)
+
+    def get_patch(self, image_list, patch_size):
+        i = 0
+        h, w = image_list[0].shape[:2]
+        rr = random.randint(0, h-patch_size)
+        cc = random.randint(0, w-patch_size)
+        for img in image_list:
+            image_list[i] = img[rr:rr+patch_size, cc:cc+patch_size, :]
+            i += 1
+        return image_list
+
+    def pad_img(self, img_list, patch_size, block_size=8):
+        i = 0
+        for img in img_list:
+            img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+            h, w = img.shape[:2]
+            bottom = 0
+            right = 0
+            if h < patch_size:
+                bottom = patch_size-h
+                h = patch_size
+            if w < patch_size:
+                right = patch_size-w
+                w = patch_size
+            bottom = bottom + (h // block_size) * block_size + \
+                (block_size if h % block_size != 0 else 0) - h
+            right = right + (w // block_size) * block_size + \
+                (block_size if w % block_size != 0 else 0) - w
+            img_list[i] = cv2.copyMakeBorder(
+                img, 0, bottom, 0, right, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+            i += 1
+        return img_list
+
+    def get_pad_size(self, index, block_size=8):
+        img = Image.open(self.input[index])
+        patch_size = self.image_size
+        img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+        h, w = img.shape[:2]
+        bottom = 0
+        right = 0
+        if h < patch_size:
+            bottom = patch_size-h
+            h = patch_size
+        if w < patch_size:
+            right = patch_size-w
+            w = patch_size
+        bottom = bottom + (h // block_size) * block_size + \
+            (block_size if h % block_size != 0 else 0) - h
+        right = right + (w // block_size) * block_size + \
+            (block_size if w % block_size != 0 else 0) - w
+        return [bottom, right]
---
+++ test.py
@@ -0,0 +1,124 @@
+import os
+import sys
+
+from src.denoising_diffusion_pytorch import GaussianDiffusion
+from src.residual_denoising_diffusion_pytorch import (ResidualDiffusion,
+                                                      Trainer, Unet, UnetRes,
+                                                      set_seed)
+
+# init 
+os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(e) for e in [0])
+sys.stdout.flush()
+set_seed(10)
+debug = False
+if debug:
+    save_and_sample_every = 2
+    sampling_timesteps = 10
+    sampling_timesteps_original_ddim_ddpm = 10
+    train_num_steps = 200
+else:
+    save_and_sample_every = 1000
+    sampling_timesteps = 5
+    sampling_timesteps_original_ddim_ddpm = 250
+    train_num_steps = 30000
+
+original_ddim_ddpm = False
+if original_ddim_ddpm:
+    condition = False
+    input_condition = False
+    input_condition_mask = False
+else:
+    condition = True
+    input_condition = True 
+    input_condition_mask = True
+
+if condition:
+    if input_condition:
+        folder = ["/home/sss/python/dataset/VOC2012_ORI/train/gt",
+                "/home/sss/python/dataset/VOC2012_ORI/train/evcs",
+                "/home/sss/python/dataset/VOC2012_ORI/train/wsobel",
+                "/home/sss/python/dataset/VOC2012_ORI/valid/gt",
+                "/home/sss/python/dataset/VOC2012_ORI/valid/evcs",
+                "/home/sss/python/dataset/VOC2012_ORI/valid/wsobel"
+                ]
+    else:
+        folder = ["/home/sss/python/dataset/Celebrity Face Image Dataset/train/gt",
+                "/home/sss/python/dataset/Celebrity Face Image Dataset/train/input_ht_rgb",
+                "/home/sss/python/dataset/Celebrity Face Image Dataset/test/gt",
+                "/home/sss/python/dataset/Celebrity Face Image Dataset/test/input_ht_rgb"]
+    train_batch_size = 1
+    num_samples = 1
+    sum_scale = 1
+    image_size = 256
+else:
+    folder = '/home/sss/python/dataset/CelebA/img_align_celeba'
+    train_batch_size = 32
+    num_samples = 25
+    sum_scale = 1
+    image_size = 32
+
+if original_ddim_ddpm:
+    model = Unet(
+        dim = 64,
+        dim_mults = (1, 2, 4, 8)
+    )
+    diffusion = GaussianDiffusion(
+        model,
+        image_size=image_size,
+        timesteps=1000,           # number of steps
+        sampling_timesteps=sampling_timesteps_original_ddim_ddpm,
+        loss_type='l1',            # L1 or L2
+    )
+else:
+    model = UnetRes(
+        dim=64,
+        dim_mults=(1, 2, 4, 8),
+        share_encoder=0, #1 0 -1，分别对应共享编码器、两个独立的 U-Net 和一个独立的 U-Net。
+        condition=condition,
+        input_condition=input_condition
+    )
+    diffusion = ResidualDiffusion(
+        model,
+        image_size=image_size,
+        timesteps=1000,           # number of steps
+        # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+        sampling_timesteps=sampling_timesteps,
+        objective='pred_res_noise', # pred_res_noise, pred_res, pred_noise
+        loss_type='l1',            # L1 or L2 or huber(SmoothL1)
+        condition=condition,
+        sum_scale = sum_scale,
+        input_condition=input_condition,
+        input_condition_mask=input_condition_mask
+    )
+
+trainer = Trainer(
+    diffusion,
+    folder,
+    train_batch_size=train_batch_size,
+    num_samples=num_samples,
+    train_lr=8e-5,
+    train_num_steps=train_num_steps,         # total training steps
+    gradient_accumulate_every=2,    # gradient accumulation steps
+    ema_decay=0.995,                # exponential moving average decay
+    amp=False,                        # turn on mixed precision
+    convert_image_to="RGB",
+    condition=condition,
+    save_and_sample_every=save_and_sample_every,
+    equalizeHist=False,
+    crop_patch=False,
+    generation = False,
+    halftone = None,  # fs, evcs, gmevcs
+    gaussian_filter = True,
+    get_sobel = None,  # None, sobel, canny, wsobel
+)
+
+if not trainer.accelerator.is_local_main_process:
+    pass
+else:
+    epoch = 30
+    trainer.load(epoch)
+    trainer.set_results_folder(f'./results/DiffSo_EVCS_Gaussian_ts{sampling_timesteps}_ep{epoch}')
+    trainer.test(last=True)
+
+# trainer.set_results_folder('./results/test_sample')
+# trainer.test(sample=True)
